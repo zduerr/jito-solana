6f7184f97b3f04c4a768cfc2b347ef806f77d58e
Lucas Bruder <lucas@jito.wtf>
2026-03-10 14:26:02 -0500

    check fmt

--- ci/test-checks.sh
+++ ci/test-checks.sh
@@ -26,6 +26,7 @@ fi
 # check dev-context-only-utils isn't used in normal dependencies
 _ scripts/check-dev-context-only-utils.sh tree
 
+cargo "+${rust_nightly}" fmt --version
 # fmt
 _ scripts/cargo-for-all-lock-files.sh -- "+${rust_nightly}" fmt --all -- --check
 
