284ce338797df65a4bee966c66b35de39e29d0d6
Lucas Bruder <lucas@jito.wtf>
2026-03-10 14:49:38 -0500

    ci

--- version/src/v4.rs
+++ version/src/v4.rs
@@ -307,7 +307,7 @@ impl Serialize for Version {
 
         let (packed_minor, patch) = PackedMinor::try_pack(minor, patch, prerelease)
             .map_err(|err| S::Error::custom(format!("{err:?}")))?;
-        let client = u16::try_from(client.clone()).map_err(S::Error::custom)?;
+        let client = u16::try_from(*client).map_err(S::Error::custom)?;
 
         let serialized_version = SerializedVersion {
             major,
